datafl4sh/diskpp

commit 19d0cfbaf7cf388b2bc05d2d1fdb8dadbe5d28ed

Moved neighbour_connectivity::neighbour_via() to std::optional.

libdiskpp/include/diskpp/loaders/loader_utils.hpp
@@ -115,7 +115,7 @@ renumber_hypercube_boundaries(Mesh& msh)
         for (const auto& fc : fcs)
         {
             auto c = cvf.neighbour_via(msh, cl, fc);
-            if (c.second)
+            if (not c)
                 continue; /* Not an external boundary */
 
             auto bn = boundary_number(normal(msh, cl, fc));

apps/diffusion/src/diffusion_dg.cpp
@@ -163,52 +163,57 @@ run_diffusion_solver(Mesh& msh, size_t degree, const typename Mesh::coordinate_t
         auto fcs = faces(msh, tcl);
         for (auto& fc : fcs)
         {
-            matrix_type Att = matrix_type::Zero(tbasis.size(), tbasis.size());
-            matrix_type Atn = matrix_type::Zero(tbasis.size(), tbasis.size());
-            
             auto nv = cvf.neighbour_via(msh, tcl, fc);
-            auto ncl = nv.first;
-            auto nbasis = disk::make_scalar_monomial_basis(msh, ncl, degree);
-            assert(tbasis.size() == nbasis.size());
             
             auto n     = normal(msh, tcl, fc);
             auto eta_l = eta / diameter(msh, fc);
             auto f_qps = disk::integrate(msh, fc, 2*degree);
             
-            for (auto& fqp : f_qps)
-            {
-                auto ep     = fqp.point();
-                auto tphi   = tbasis.eval_functions(ep);
-                auto tdphi  = tbasis.eval_gradients(ep);
+            if (nv) {
+                matrix_type Att = matrix_type::Zero(tbasis.size(), tbasis.size());
+                matrix_type Atn = matrix_type::Zero(tbasis.size(), tbasis.size());
                 
-                if (nv.second)
-                {   /* NOT on a boundary */
+                auto ncl = nv.value();
+                auto nbasis = disk::make_scalar_monomial_basis(msh, ncl, degree);
+                assert(tbasis.size() == nbasis.size());
+                
+                for (auto& fqp : f_qps) {
+                    auto ep     = fqp.point();
+                    auto tphi   = tbasis.eval_functions(ep);
+                    auto tdphi  = tbasis.eval_gradients(ep);
+                
+                    /* NOT on a boundary */
                     Att += + fqp.weight() * eta_l * tphi * tphi.transpose();
                     Att += - fqp.weight() * 0.5 * tphi * (tdphi*n).transpose();
                     Att += - fqp.weight() * 0.5 * (tdphi*n) * tphi.transpose();
+                
+                    auto nphi   = nbasis.eval_functions(ep);
+                    auto ndphi  = nbasis.eval_gradients(ep);
+                
+                    Atn += - fqp.weight() * eta_l * tphi * nphi.transpose();
+                    Atn += - fqp.weight() * 0.5 * tphi * (ndphi*n).transpose();
+                    Atn += + fqp.weight() * 0.5 * (tdphi*n) * nphi.transpose();
                 }
-                else
-                {   /* On a boundary*/
+                assm.assemble(msh, tcl, tcl, Att);
+                assm.assemble(msh, tcl, ncl, Atn);
+            }
+            else {
+                matrix_type Att = matrix_type::Zero(tbasis.size(), tbasis.size());
+                for (auto& fqp : f_qps) {
+                    auto ep     = fqp.point();
+                    auto tphi   = tbasis.eval_functions(ep);
+                    auto tdphi  = tbasis.eval_gradients(ep);
+                    
+                    /* On a boundary*/
                     Att += + fqp.weight() * eta_l * tphi * tphi.transpose();
                     Att += - fqp.weight() * tphi * (tdphi*n).transpose();
                     Att += - fqp.weight() * (tdphi*n) * tphi.transpose();
                     
                     //loc_rhs -= fqp.weight() * (tdphi*n);
                     //loc_rhs += fqp.weight() * eta_l * tphi;
-                    continue;
                 }
-                
-                auto nphi   = nbasis.eval_functions(ep);
-                auto ndphi  = nbasis.eval_gradients(ep);
-                
-                Atn += - fqp.weight() * eta_l * tphi * nphi.transpose();
-                Atn += - fqp.weight() * 0.5 * tphi * (ndphi*n).transpose();
-                Atn += + fqp.weight() * 0.5 * (tdphi*n) * nphi.transpose();
-            }
-            
-            assm.assemble(msh, tcl, tcl, Att);
-            if (nv.second)
-                assm.assemble(msh, tcl, ncl, Atn);
+                assm.assemble(msh, tcl, tcl, Att);
+            }   
         }
         
         assm.assemble(msh, tcl, K, loc_rhs);

apps/hho_dd/src/hhodd.cpp
@@ -29,7 +29,7 @@ make_overlapping_subdomains(const Mesh& msh, size_t overlap_layers)
      * precondition for std::lower_bound() below. */
 
     for (size_t ol = 0; ol < overlap_layers; ol++) {
-        std::map<size_t, std::vector<cell_type>> layer_cells;
+        std::map<size_t, std::set<cell_type>> layer_cells;
         /* Iterate on the currently identified subdomains */
         for (auto& [tag, cells] : subdomains) {
             /* for each cell */
@@ -37,25 +37,22 @@ make_overlapping_subdomains(const Mesh& msh, size_t overlap_layers)
                 auto fcs = faces(msh, cl);
                 for (auto& fc : fcs) {
                     /* for each neighbour */
-                    auto [neigh, valid] = cvf.neighbour_via(msh, cl, fc);
-                    if (!valid)
-                        continue;
+                    auto neigh_opt = cvf.neighbour_via(msh, cl, fc);
+                    if (neigh_opt) {
+                        /* and check if it is already in the current subdomain */
+                        bool present = std::binary_search(cells.begin(),
+                            cells.end(), neigh_opt.value());
 
-                    /* and check if it is already in the current subdomain */
-                    bool present = std::binary_search(cells.begin(), cells.end(), neigh);
-
-                    /* if not, save it */
-                    if (not present)
-                        layer_cells[tag].push_back(neigh);
+                        /* if not, save it */
+                        if (not present)
+                            layer_cells[tag].insert(neigh_opt.value());
+                    }
                 }
             }
         }
 
         /* move the found neighbours to the subdomain */
         for (auto& [tag, cells] : layer_cells) {
-            std::sort(cells.begin(), cells.end());
-            auto last = std::unique(cells.begin(), cells.end());
-            cells.erase(last, cells.end());
             subdomains[tag].insert(subdomains[tag].end(), cells.begin(), cells.end());
             std::sort(subdomains[tag].begin(), subdomains[tag].end());
         }

apps/maxwell/src/maxwell_sip_dg.cpp
@@ -238,41 +238,61 @@ run_maxwell_solver(Mesh& msh, size_t degree, const typename Mesh::coordinate_typ
                 return pl.plane_wave_source(bi.tag(), pt);
             };
 
-            matrix_type Att = matrix_type::Zero(tbasis.size(), tbasis.size());
-            matrix_type Atn = matrix_type::Zero(tbasis.size(), tbasis.size());
-
             auto nv = cvf.neighbour_via(msh, tcl, fc);
-            auto ncl = nv.first;
-            auto nbasis = disk::make_vector_monomial_basis(msh, ncl, degree);
-            assert(tbasis.size() == nbasis.size());
 
             auto n     = normal(msh, tcl, fc);
             auto eta_l = eta / diameter(msh, fc);
             auto f_qps = disk::integrate(msh, fc, 2*degree);
 
-            for (auto& fqp : f_qps)
-            {
-                auto tphi       = tbasis.eval_functions(fqp.point());
-                auto tcphi      = tbasis.eval_curls2(fqp.point());
-                auto n_x_tphi   = disk::vcross(n, tphi);
-                auto n_x_tphi_x_n = disk::vcross(n_x_tphi, n);
+            if (nv) {
+                matrix_type Att = matrix_type::Zero(tbasis.size(), tbasis.size());
+                matrix_type Atn = matrix_type::Zero(tbasis.size(), tbasis.size());
 
-                if (nv.second)
-                {   /* NOT on a boundary */
+                auto ncl = nv.value();
+                auto nbasis = disk::make_vector_monomial_basis(msh, ncl, degree);
+                assert(tbasis.size() == nbasis.size());
+                
+                for (auto& fqp : f_qps)
+                {
+                    auto tphi       = tbasis.eval_functions(fqp.point());
+                    auto tcphi      = tbasis.eval_curls2(fqp.point());
+                    auto n_x_tphi   = disk::vcross(n, tphi);
+                    auto n_x_tphi_x_n = disk::vcross(n_x_tphi, n);
+
+                    /* NOT on a boundary */
                     Att += + fqp.weight() * eta_l * n_x_tphi * n_x_tphi.transpose();
                     Att += - fqp.weight() * 0.5 * n_x_tphi * tcphi.transpose();
                     Att += - fqp.weight() * 0.5 * tcphi * n_x_tphi.transpose();
+
+                    auto nphi       = nbasis.eval_functions(fqp.point());
+                    auto ncphi      = nbasis.eval_curls2(fqp.point());
+                    auto n_x_nphi   = disk::vcross(n, nphi);
+
+                    Atn += - fqp.weight() * eta_l * n_x_tphi * n_x_nphi.transpose();
+                    Atn += - fqp.weight() * 0.5 * n_x_tphi * ncphi.transpose();
+                    Atn += + fqp.weight() * 0.5 * tcphi * n_x_nphi.transpose();
                 }
-                else
+
+                assm.assemble(msh, tcl, tcl, Att);
+                assm.assemble(msh, tcl, ncl, Atn);
+            }
+            else {
+                matrix_type Att = matrix_type::Zero(tbasis.size(), tbasis.size());
+
+                for (auto& fqp : f_qps)
                 {
-                   /* On a boundary*/
+                    auto tphi       = tbasis.eval_functions(fqp.point());
+                    auto tcphi      = tbasis.eval_curls2(fqp.point());
+                    auto n_x_tphi   = disk::vcross(n, tphi);
+                    auto n_x_tphi_x_n = disk::vcross(n_x_tphi, n);
+                    
+                    /* On a boundary*/
                     if ( not pl.is_magnetic_like(bi.tag()) and not pl.is_impedance_like(bi.tag()) )
                     {
                         Att += + fqp.weight() * eta_l * n_x_tphi * n_x_tphi.transpose();
                         Att += - fqp.weight() * tcphi * n_x_tphi.transpose();
                         Att += - fqp.weight() * n_x_tphi * tcphi.transpose();
                     }
-
 
                     //loc_rhs -= fqp.weight() * tcphi_x_n;
                     //loc_rhs += fqp.weight() * eta_l * tphi;
@@ -282,26 +302,13 @@ run_maxwell_solver(Mesh& msh, size_t degree, const typename Mesh::coordinate_typ
 
                     if ( pl.is_plane_wave(bi.tag()) )
                         loc_rhs -= 2*fqp.weight()*(jwmu0/Z)*n_x_tphi_x_n*f(fqp.point());
-
-                    continue;
                 }
 
-                auto nphi       = nbasis.eval_functions(fqp.point());
-                auto ncphi      = nbasis.eval_curls2(fqp.point());
-                auto n_x_nphi   = disk::vcross(n, nphi);
-
-                Atn += - fqp.weight() * eta_l * n_x_tphi * n_x_nphi.transpose();
-                Atn += - fqp.weight() * 0.5 * n_x_tphi * ncphi.transpose();
-                Atn += + fqp.weight() * 0.5 * tcphi * n_x_nphi.transpose();
-            }
-
-            assm.assemble(msh, tcl, tcl, Att);
-            if (nv.second)
-                assm.assemble(msh, tcl, ncl, Atn);
+                assm.assemble(msh, tcl, tcl, Att);
+            }   
         }
 
         matrix_type LC = (1./mur)*K - (ksq*epsr - jwmu0*sigma)*M;
-
         assm.assemble(msh, tcl, LC, loc_rhs);
     }
 

libdiskpp/include/diskpp/mesh/mesh.hpp
@@ -740,11 +740,10 @@ public:
         compute_connectivity(msh);
     }
 
-    std::pair<cell_type, bool>
+    std::optional<cell_type>
     neighbour_via(const Mesh& msh, const cell_type& cl, const face_type& fc)
     {
-        if ( face_owners.size() != msh.faces_size() )
-            throw std::logic_error("Inconsistent neighbour information.");
+        assert ( face_owners.size() == msh.faces_size() );
 
         auto cl_ofs = offset(msh, cl);
         auto fc_ofs = offset(msh, fc);
@@ -757,9 +756,9 @@ public:
         assert(fo[0].value() == cl_ofs);
 
         if (not fo[1])
-            return std::make_pair(*msh.cells_begin(), false);
+            return {};
 
-        return std::make_pair( *std::next(msh.cells_begin(), fo[1].value()), true);
+        return *std::next(msh.cells_begin(), fo[1].value());
     }
 };
 

commit 2759f7a7d04074ed4dcaeede73da3c16b70cff2a

Corrected a bug introduced in 19d0cfb.

libdiskpp/include/diskpp/loaders/loader_utils.hpp
@@ -114,8 +114,8 @@ renumber_hypercube_boundaries(Mesh& msh)
         const auto fcs = faces(msh, cl);
         for (const auto& fc : fcs)
         {
-            auto c = cvf.neighbour_via(msh, cl, fc);
-            if (not c)
+            auto neigh = cvf.neighbour_via(msh, cl, fc);
+            if (neigh)
                 continue; /* Not an external boundary */
 
             auto bn = boundary_number(normal(msh, cl, fc));